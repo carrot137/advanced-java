e550a99ca7a3d98d463b01855e21adb50fbffc0c
Toida <tetsuro.toida@rakus-partners.co.jp>
2025-01-20 10:53:28 +0900

    update

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,5 +1,11 @@
 public class Ex01 {
     public static void main(String[] args) {
+        String message ="I have a dream today";
+        int stringLength=message.length();
+
+        System.out.println(stringLength);
+
+
         
     }
 
